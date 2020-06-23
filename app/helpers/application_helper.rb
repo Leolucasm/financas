d8df6700c09b47fa76f74355f7d19b7d15405a26
Leonardo Melo <leolucasm92@gmail.com>
2020-06-23 08:40:53 -0300

    Ajustes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,27 @@ module ApplicationHelper
       "<div class='badge badge-danger'>#{t('helpers.inactive')}</div>".html_safe
     end
   end
+
+  # Types: primary, secondary, success, danger, warning
+  #        info, light, dark
+  def get_alert(type, title, description)
+    "<div class='alert alert-#{type.to_s}'>
+      <div class='alert-title'>#{title}</div>
+      #{description}
+    </div>".html_safe
+  end
+
+  def get_alert_error(errors)
+    @message_error = "<ul>"
+    errors.full_messages.each do |error|
+      @message_error += "<li>#{error}</li>"
+    end
+
+    @message_error += "</ul>"
+
+    "<div class='alert alert-danger'>
+      Erros encontrados:
+      #{@message_error}
+    </div>".html_safe
+  end
 end
