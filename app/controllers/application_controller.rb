b7ef82344667579ed70c5ea0ba60581f248e8ade
Leonardo Melo <leonardo@ibfsistemas.com.br>
2021-01-29 18:13:46 -0300

    Adicionar nome ao usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   require './lib/modal_responder'
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def respond_modal_with(*args, &blk)
     options = args.extract_options!
     options[:responder] = ModalResponder
@@ -14,5 +16,12 @@ class ApplicationController < ActionController::Base
         value.gsub!(/,/,'.') # '1,23' -> '1.23'
       end
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates_length_of :name, minimum: 3, maximum: 255, allow_blank: false
 end
